a890e762d17585b2b6b523e1555bfa9647b96bc6
adr <senhoraroldo@gmail.com>
2021-03-11 14:44:50 -0300

    merge

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,7 +1,8 @@
 function verificaFullPage() {
     const tamanho = window.matchMedia('(min-width: 992px)')
+    let alturaTela = window.innerHeight;
 
-    if (window.innerHeight > 640 && tamanho) {
+    if (alturaTela > 640 && alturaTela < 1000 && tamanho) {
         new fullpage('#fullpage', {
             //options here
             autoScrolling: true,
