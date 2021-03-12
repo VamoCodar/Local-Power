d5e604784a842c53a713998e05f1069759e0d08a
hahayz77 <danilo__danilo@hotmail.com>
2021-03-12 16:10:31 -0300

    swipperjs

--- assets/js/swipper.js
+++ assets/js/swipper.js
@@ -1,10 +1,9 @@
 const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     effect: 'fade',
     autoplay: {
-      delay: 3000,
-      disableOnInteraction: true,
-    },
-  });
+        delay: 3000,
+        disableOnInteraction: true,
+    }
+});
