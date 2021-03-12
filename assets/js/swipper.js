570dbcc104b924b7d3f6f8ffa124615787b59572
hahayz77 <danilo__danilo@hotmail.com>
2021-03-12 16:42:30 -0300

    correção swiper

--- assets/js/swipper.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    effect: 'fade',
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: true,
-    }
-});
