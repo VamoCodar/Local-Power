c27d7a64c82af5306dc4bb26ef8e120c4ff1ffc4
hahayz77 <danilo__danilo@hotmail.com>
2021-03-12 15:23:35 -0300

    erro slider

---
+++ assets/js/swipper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'fade',
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: true,
+    // },
+  });
